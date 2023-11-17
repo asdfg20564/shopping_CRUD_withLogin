9feaeb6e326311ef0b0a4d04cf79c0c500fce6cf
asdfg20564 <44521546+asdfg20564@users.noreply.github.com>
2023-11-17 16:00:06 +0900

    salt key fixed
    salt key fixed

--- models/user.js
+++ models/user.js
@@ -40,10 +40,8 @@ module.exports = (sequelize, DataTypes) => {
 
   User.beforeCreate(async (user) => {
     //salt will be generated with the specified number of rounds and used.
-    const hashedPasswd = await bcrypt.hash(
-      user.passwd,
-      process.env.SALT_ROUND_KEY
-    );
+    const salyRoundKey = parseInt(process.env.SALT_ROUND_KEY);
+    const hashedPasswd = await bcrypt.hash(user.passwd, salyRoundKey);
     user.passwd = hashedPasswd;
   });
   return User;
